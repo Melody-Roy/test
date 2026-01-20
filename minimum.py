d8c187338f919d4d5d50d36176fa3d4b5e9a0df8
Melody-Roy <melodyroy@cegepsth.qc.ca>
2026-01-20 09:04:24 -0500

    Implement minimum_de_trois function
    Add a function to find the minimum of three numbers.

---
+++ minimum.py
@@ -0,0 +1,14 @@
+def minimum_de_trois(a, b, c):
+    """
+    Retourne la valeur minimale parmi trois nombres.
+
+    Paramètres:
+        a (float): premier nombre
+        b (float): deuxième nombre
+        c (float): troisième nombre
+
+    Retour:
+        float: le plus petit des trois nombres
+    """
+    # TODO: Remplacer 'pass' par votre code
+    pass
